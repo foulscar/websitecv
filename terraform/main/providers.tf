8cc4b53925ff9d3cae9fdef3aa9dccabc57510f2
Corbin Grossen <corbingrossen@gmail.com>
2023-12-20 17:11:55 -0500

    change tfc variables v3

--- terraform/main/providers.tf
+++ terraform/main/providers.tf
@@ -2,6 +2,8 @@
 // This TF File will Assume Different IAM Roles for each Stage
 // ---
 
+
+// Main Account
 provider "aws" {
   shared_config_files = [var.tfc_aws_dynamic_credentials.default.shared_config_file]
 }
