407f81c940de50aa15888adc8c93c0ea83962128
Corbin Grossen <corbingrossen@gmail.com>
2023-12-20 17:05:05 -0500

    change tfc variables v2

--- terraform/main/vars.tf
+++ terraform/main/vars.tf
@@ -1,3 +1,15 @@
+variable "tfc_aws_dynamic_credentials" {
+  description = "Object containing AWS dynamic credentials configuration"
+  type = object({
+    default = object({
+      shared_config_file = string
+    })
+    aliases = map(object({
+      shared_config_file = string
+    }))
+  })
+}
+
 variable "TFC_AWS_PROVIDER_AUTH" {
   type = string
   default = "true"
