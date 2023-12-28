f10d82b53bce4367a4ed7848657f7ca27122b6e2
Corbin Grossen <corbingrossen@gmail.com>
2023-12-27 20:01:49 -0500

    route 53 init implementation

--- terraform/main/route53.tf
+++ terraform/main/route53.tf
@@ -8,8 +8,8 @@ resource "aws_route53_zone" "main" {
 
 resource "aws_route53_record" "domain_validation_record" {
   zone_id = aws_route53_zone.main.zone_id
-  name = aws_acm_certificate.domain_certificate_request.domain_validation_options.resource_record_name
-  type = aws_acm_certificate.domain_certificate_request.domain_validation_options.resource_record_type
-  records = [aws_acm_certificate.domain_certificate_request.domain_validation_options.resource_record_value]
+  name = aws_acm_certificate.domain_certificate_request.domain_validation_options.0.resource_record_name
+  type = aws_acm_certificate.domain_certificate_request.domain_validation_options.0.resource_record_type
+  records = [aws_acm_certificate.domain_certificate_request.domain_validation_options.0.resource_record_value]
   ttl = 300
 }
