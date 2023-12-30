5f51838e89e9e4d3cdd022c2d87110d6e6421b98
Corbin Grossen <corbingrossen@gmail.com>
2023-12-30 12:27:49 -0500

    Make the IONOS Custom NameServers Dynamic to Route53

--- terraform/main/ionos.tf
+++ terraform/main/ionos.tf
@@ -2,16 +2,21 @@
 // Use the IONOS Developer API to Change the Domain Name Servers
 // ---
 
-data "aws_secretsmanager_secret" "secrets" {
+data "aws_secretsmanager_secret" "ionos_developer_key" {
   arn = var.IONOS_KEY_ARN
 }
 
-data "aws_secretsmanager_secret_version" "current" {
-  secret_id = data.aws_secretsmanager_secret.secrets.id
+data "aws_secretsmanager_secret_version" "ionos_developer_key_ver" {
+  secret_id = data.aws_secretsmanager_secret.ionos_developer_key.id
 }
 
 resource "null_resource" "get_ionos_data" {
   provisioner "local-exec" {
-    command = "curl -X 'GET' 'https:/api.hosting.ionos.com/domains/v1/domainitems/${var.IONOS_DOMAIN_ID}/nameservers' -H 'accept: application/json' -H 'X-Api-Key: ${jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)}'"
+    command = <<-EOT
+      curl -X 'GET' \n
+      'https:/api.hosting.ionos.com/domains/v1/domainitems/${var.IONOS_DOMAIN_ID}/nameservers' \n
+      -H 'accept: application/json' \n
+      -H 'X-Api-Key: ${data.aws_secretsmanager_secret_version.ionos_developer_key_ver.secret_string}'
+    EOT
   }
 }
