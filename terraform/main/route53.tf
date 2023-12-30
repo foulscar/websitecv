492e2abf3b69f4e3aae7c9f85a276a5492198f14
Corbin Grossen <corbingrossen@gmail.com>
2023-12-30 07:47:14 -0500

    implement subdomain for dev

--- terraform/main/route53.tf
+++ terraform/main/route53.tf
@@ -6,22 +6,6 @@ resource "aws_route53_zone" "main" {
   name = var.domain_name
 }
 
-resource "aws_route53_record" "domain_validation_record" {
-  for_each = {
-    for dvo in aws_acm_certificate.domain_certificate_request.domain_validation_options: dvo.domain_name => {
-      name = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type = dvo.resource_record_type
-    }
-  }
-  zone_id = aws_route53_zone.main.zone_id
-  name = each.value.name
-  type = each.value.type
-  records = [each.value.record]
-  ttl = 60
-  allow_overwrite = true
-}
-
 // ---
 // Dev
 // ---
