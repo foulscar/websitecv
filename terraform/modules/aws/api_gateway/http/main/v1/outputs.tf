6b342635437041bcfc3d65f47cb52b668cc28b07
Corbin Grossen <corbingrossen@gmail.com>
2024-01-01 20:31:00 -0500

    Fix Specific Instances (count) error on the HTTP API module)

--- terraform/modules/aws/api_gateway/http/main/v1/outputs.tf
+++ terraform/modules/aws/api_gateway/http/main/v1/outputs.tf
@@ -16,9 +16,9 @@ output "api_endpoint" {
 }
 
 output "target_domain_name" {
-  value = aws_apigatewayv2_domain_name.domain_name.domain_name_configuration[0].target_domain_name
+  value = aws_apigatewayv2_domain_name.domain_name[0].domain_name_configuration[0].target_domain_name
 }
 
 output "hosted_zone_id" {
-  value = aws_apigatewayv2_domain_name.domain_name.domain_name.domain_name_configuration[0].hosted_zone_id
+  value = aws_apigatewayv2_domain_name.domain_name[0].domain_name.domain_name_configuration[0].hosted_zone_id
 }
