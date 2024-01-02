a1bac7b6560a351d679f13b9f5f4b42f682c3aea
Corbin Grossen <corbingrossen@gmail.com>
2024-01-01 20:33:06 -0500

    Fixed Typo

--- terraform/modules/aws/api_gateway/http/main/v1/outputs.tf
+++ terraform/modules/aws/api_gateway/http/main/v1/outputs.tf
@@ -20,5 +20,5 @@ output "target_domain_name" {
 }
 
 output "hosted_zone_id" {
-  value = aws_apigatewayv2_domain_name.domain_name[0].domain_name.domain_name_configuration[0].hosted_zone_id
+  value = aws_apigatewayv2_domain_name.domain_name[0].domain_name_configuration[0].hosted_zone_id
 }
