5ef04c94fa230b4fc931ff774edf6d186570a9d7
Corbin Grossen <corbingrossen@gmail.com>
2024-01-02 12:50:03 -0500

    Init GitHub Provider

--- terraform/stages/dev/github.tf
+++ terraform/stages/dev/github.tf
@@ -5,3 +5,9 @@
 data "github_actions_public_key" "repo_public_key" {
   repository = var.gh_repo_name
 }
+
+resource "github_actions_secret" "bucket" {
+  repository = var.gh_repo_name
+  secret_name = "${var.stage}_html_s3_bucket"
+  plaintext_value = module.s3_bucket.s3_bucket_domain_name
+}
