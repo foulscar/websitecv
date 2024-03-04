3ba8aaf3c47dd9c309f8d33d4cba965dc9375ae0
Corbin Grossen <corbingrossen@gmail.com>
2024-03-03 21:34:38 -0500

    Destroy DEV stage to save my wallet

--- terraform/main/outputs.tf
+++ terraform/main/outputs.tf
@@ -2,7 +2,3 @@ output "nameservers" {
   value = aws_route53_zone.main.name_servers
   description = "Use these NameServers for the Domain"
 }
-
-output "dev-nameservers" {
-  value = module.stage_dev.subdomain_ns
-}
